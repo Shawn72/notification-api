8ce94b38b17d2e9ab33fa90dfd3953c6a1dadb95
Shawn Andre Mbuvi <shawnmbuvi@gmail.com>
2022-03-02 16:51:34 +0300

    first api push

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@
 
 FROM mcr.microsoft.com/dotnet/aspnet:5.0 AS base
 WORKDIR /app
-EXPOSE 80
+EXPOSE 8036
 EXPOSE 443
 
 FROM mcr.microsoft.com/dotnet/sdk:5.0 AS build
@@ -19,4 +19,4 @@ RUN dotnet publish "NotificationApi.csproj" -c Release -o /app/publish
 FROM base AS final
 WORKDIR /app
 COPY --from=publish /app/publish .
-ENTRYPOINT ["dotnet", "NotificationApi.dll"]
+ENTRYPOINT ["dotnet", "NotificationApi.dll",  "--environment=Production"]
--- Repository/CustomerAlertSettingRepository.cs
+++ Repository/CustomerAlertSettingRepository.cs
@@ -41,6 +41,26 @@ namespace NotificationApi.Repository
         public async Task UpdateCustomerAlertSetting(CustomerAlertSetting customerAlertsetting, CustomerAlertSetting dBcustomerAlertsetting)
         {
             dBcustomerAlertsetting.customer_name = customerAlertsetting.customer_name;
+            dBcustomerAlertsetting.percentage_alert = customerAlertsetting.percentage_alert;
+            dBcustomerAlertsetting.email_address = customerAlertsetting.email_address;
+            dBcustomerAlertsetting.pol_id = customerAlertsetting.pol_id;
+            dBcustomerAlertsetting.pool_number= customerAlertsetting.pool_number;
+            dBcustomerAlertsetting.country_code= customerAlertsetting.country_code;
+            dBcustomerAlertsetting.inpatient_alert= customerAlertsetting.inpatient_alert;
+            dBcustomerAlertsetting.inpatient_weekly_alert = customerAlertsetting.inpatient_weekly_alert;
+            dBcustomerAlertsetting.customer_code= customerAlertsetting.customer_code;
+            dBcustomerAlertsetting.member_perc_util=customerAlertsetting.member_perc_util;
+            dBcustomerAlertsetting.scheme_util_index = customerAlertsetting.scheme_util_index;
+            dBcustomerAlertsetting.ip_alert_email = customerAlertsetting.ip_alert_email;
+            dBcustomerAlertsetting.full_member_util = customerAlertsetting.full_member_util;
+            dBcustomerAlertsetting.full_member_util_shared = customerAlertsetting.full_member_util_shared;
+            dBcustomerAlertsetting.full_member_util_email = customerAlertsetting.full_member_util_email;
+            dBcustomerAlertsetting.scheme_expiry_alert_email = customerAlertsetting.scheme_expiry_alert_email;
+            dBcustomerAlertsetting.age_cut_off_alert = customerAlertsetting.age_cut_off_alert;
+            dBcustomerAlertsetting.split_report_into_multiple_sheets = customerAlertsetting.split_report_into_multiple_sheets;
+            dBcustomerAlertsetting.perc_report_frequency = customerAlertsetting.perc_report_frequency;
+            dBcustomerAlertsetting.include_cat_desc = customerAlertsetting.include_cat_desc;
+            dBcustomerAlertsetting.include_util_summary = customerAlertsetting.include_util_summary;
             //update with coulumns you'd like to update
             await _context.SaveChangesAsync();
         }
